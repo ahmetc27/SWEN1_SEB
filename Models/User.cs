9fdbaf2ce89a5471c8bb41902e87d7eb8090322b
Cicek <ahmetcicek085@gmail.com>
2025-03-11 18:29:27 +0100

    refactor: use 'string.Empty' instead of 'String.Empty' for consistency

--- Models/User.cs
+++ Models/User.cs
@@ -2,8 +2,8 @@ namespace SEB.Models
 {
     public class User
     {
-        public string Username { get; set; } = String.Empty;
-        public string Password {get; set; } = String.Empty;
+        public string Username { get; set; } = string.Empty;
+        public string Password {get; set; } = string.Empty;
         public int Elo { get; set; } = 0;
         public List<PushUpRecord> PushUpHistory { get; set; } = new List<PushUpRecord>();
 
